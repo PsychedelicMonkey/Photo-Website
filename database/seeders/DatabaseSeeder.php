c54431a22cceca2425d8b328cbb7bef3efc26a00
PsychedelicMonkey <ryan.getz.96@gmail.com>
2025-02-16 08:32:38 -0700

    Seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,14 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Closure;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\Console\Helper\ProgressBar;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +19,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        DB::raw('SET time_zone=\'+00:00\'');
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->command->warn(PHP_EOL . 'Creating admin user...');
+        $user = $this->withProgressBar(1, fn () => User::factory(1)->create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+        ]));
+        $this->command->info('Admin user created.');
+    }
+
+    /** @return Collection<string, Model> */
+    protected function withProgressBar(int $amount, Closure $createCollectionOfOne): Collection
+    {
+        $progressBar = new ProgressBar($this->command->getOutput(), $amount);
+
+        $progressBar->start();
+
+        $items = new Collection;
+
+        foreach (range(1, $amount) as $item) {
+            $items = $items->merge(
+                $createCollectionOfOne()
+            );
+        }
+
+        $progressBar->finish();
+
+        $this->command->getOutput()->writeln('');
+
+        return $items;
     }
 }
