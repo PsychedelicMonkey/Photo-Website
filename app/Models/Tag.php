ff6b51c0707e9f5d0ce7ae553b22a0d87eb6a008
PsychedelicMonkey <ryan.getz.96@gmail.com>
2025-02-16 12:41:19 -0700

    Fix tag model

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,22 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Spatie\EloquentSortable\Sortable;
-use Spatie\EloquentSortable\SortableTrait;
+use Spatie\Tags\Tag as BaseTag;
 
-class Tag extends Model implements Sortable
+class Tag extends BaseTag
 {
     /** @use HasFactory<\Database\Factories\TagFactory> */
     use HasFactory;
     use HasUlids;
-    use SortableTrait;
-
-    /**
-     * @var array<string, mixed>
-     */
-    public $sortable = [
-        'order_column_name' => 'order_column',
-        'sort_when_creating' => true,
-    ];
 }
