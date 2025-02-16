14a985438f784729d9a7417429bda228b376b2da
PsychedelicMonkey <ryan.getz.96@gmail.com>
2025-02-16 08:26:36 -0700

    ULIDs

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,15 +3,36 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Carbon\CarbonInterface;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\MassPrunable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @property string $id
+ * @property string $name
+ * @property string $email
+ * @property ?CarbonInterface $email_verified_at
+ * @property string $password
+ * @property ?string $remember_token
+ * @property CarbonInterface $created_at
+ * @property CarbonInterface $updated_at
+ * @property ?CarbonInterface $deleted_at
+ */
 class User extends Authenticatable
 {
+    use HasApiTokens;
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
+    use HasUlids;
+    use MassPrunable;
+    use Notifiable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -21,7 +42,9 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'email_verified_at',
         'password',
+        'remember_token',
     ];
 
     /**
@@ -45,5 +68,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Get the prunable model query.
+     *
+     * @return Builder<User>
+     */
+    public function prunable(): Builder
+    {
+        return static::where('deleted_at', '<=', now()->subMonth());
     }
 }
