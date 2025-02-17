f3d57ca7c320f96077c53030c167e90b45478592
PsychedelicMonkey <ryan.getz.96@gmail.com>
2025-02-16 20:26:44 -0700

    Frontend layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    <h1 class="text-3xl font-semibold text-gray-900 dark:text-gray-100">{{ __("Home Page") }}</h1>
+</x-app-layout>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+        />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
+
+        @livewireStyles
+
+        <title>{{ config("app.name") }}</title>
+
+        @vite(["resources/css/app.css", "resources/js/app.js"])
+    </head>
+    <body>
+        <div class="min-h-screen">
+            @include("layouts.navigation")
+
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+
+        @livewireScripts
+    </body>
+</html>
